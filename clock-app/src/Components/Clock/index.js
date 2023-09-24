4ffa25b360dad89085cbbd931a2b20fbe97d1cd8
Samanvith <Samanvith2005@gmail.com>
2023-09-24 12:30:01 +0530

    Added comments

--- clock-app/src/Components/Clock/index.js
+++ clock-app/src/Components/Clock/index.js
@@ -2,15 +2,24 @@ import {Component} from 'react'
 import './index.css'
 
 class Clock extends Component {
-  constructor(props) {
+  constructor(props) {  //The constructor() method is used to set up the initial state and class variables.
     super(props)
     this.state = { date: new Date() }
+    //  We must call the super(props) method before any other statement. Calling super(props) makes sure that constructor() of the React.Component gets called and initializes the instance.
+
+
   }
   componentDidMount() {
     this.timerID = setInterval(this.tick, 1000)
+    //The componentDidMount() method is used to run statements that require that the component is already placed in the DOM.
+
+//Example: set timers, initiate API calls, etc.
+
+
   }
   componentWillUnmount() {
     clearInterval(this.timerID)
+    //The componentWillUnmount() method is used to cleanup activities performed.
   }
   tick = () => {
     this.setState({
